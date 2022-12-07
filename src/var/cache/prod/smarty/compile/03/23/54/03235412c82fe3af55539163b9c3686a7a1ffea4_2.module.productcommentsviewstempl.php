6f18f946db97daa3c121c85f8bb12c676bcf76ff
mmatuszewsky <maciasmax10@gmail.com>
2022-12-07 14:59:25 +0100

    Add presta source files vol.3

---
+++ src/var/cache/prod/smarty/compile/03/23/54/03235412c82fe3af55539163b9c3686a7a1ffea4_2.module.productcommentsviewstempl.php
@@ -0,0 +1,33 @@
+<?php
+/* Smarty version 3.1.43, created on 2022-12-07 12:24:15
+  from 'module:productcommentsviewstempl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.43',
+  'unifunc' => 'content_639077df5550a1_20178920',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '03235412c82fe3af55539163b9c3686a7a1ffea4' => 
+    array (
+      0 => 'module:productcommentsviewstempl',
+      1 => 1644505599,
+      2 => 'module',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_639077df5550a1_20178920 (Smarty_Internal_Template $_smarty_tpl) {
+if ($_smarty_tpl->tpl_vars['nb_comments']->value != 0) {?>
+  <div class="comments-note">
+    <span><?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['l'][0], array( array('s'=>'Grade','d'=>'Modules.Productcomments.Shop'),$_smarty_tpl ) );?>
+</span>
+    <div class="grade-stars" data-grade="<?php echo htmlspecialchars($_smarty_tpl->tpl_vars['grade']->value, ENT_QUOTES, 'UTF-8');?>
+"></div>
+  </div>
+<?php }
+}
+}
