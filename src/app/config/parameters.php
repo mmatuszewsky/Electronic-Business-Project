47eec64701fc26f8aad057afeb36419afb472ee2
zielu <73025866+MDG369@users.noreply.github.com>
2023-01-17 17:32:56 +0100

    updated host name

--- src/app/config/parameters.php
+++ src/app/config/parameters.php
@@ -1,7 +1,7 @@
 <?php return array (
   'parameters' => 
   array (
-    'database_host' => 'mariadb',
+    'database_host' => 'BE_184372_mariadb',
     'database_port' => '',
     'database_name' => 'prestashop',
     'database_user' => 'root',
